759f6c2ecdbfb6fda56c31d6849b7188fbebf1bb
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-12-27 22:05:19 +0200

    commit6

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, NavLink, useNavigate } from 'react-router-dom';
+import { useParams, NavLink, useNavigate, useLocation } from 'react-router-dom';
 import MovieDetailsCss from './MovieDetailsCss.module.css';
 
 const MovieDetails = () => {
   const [movieDetails, setMovieDetails] = useState(null);
   const { movieId } = useParams();
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
@@ -30,7 +31,24 @@ const MovieDetails = () => {
   }, [movieId]);
 
   const handleGoBack = () => {
-    navigate(-1); 
+    const currentPath = location.pathname;
+
+    // Перевірка, чи поточна сторінка - це сторінка Home або список фільмів на сторінці Home
+    const isHomePage = currentPath === '/' || currentPath.startsWith('/movies/');
+    if (isHomePage) {
+      navigate('/');
+      return;
+    }
+
+    // Перевірка, чи поточна сторінка - це сторінка фільмів на сторінці Movies
+    const isMoviesPage = currentPath.includes('/movies');
+    if (isMoviesPage) {
+      navigate('/movies');
+      return;
+    }
+
+    // За замовчуванням, якщо не виконується жодна з умов, повернення на головну сторінку
+    navigate('/');
   };
 
   if (!movieDetails) {
@@ -42,21 +60,27 @@ const MovieDetails = () => {
   return (
     <div className={MovieDetailsCss.container}>
       <div className={MovieDetailsCss.cont}>
-      <div className={MovieDetailsCss.btnImg}>
-        <button className={MovieDetailsCss.goBackButton} onClick={handleGoBack}>Go back</button>
-        {movieDetails.poster_path && (
-          <img className={MovieDetailsCss.img} src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`} alt={movieDetails.title} />
-        )}
+        <div className={MovieDetailsCss.btnImg}>
+          <button className={MovieDetailsCss.goBackButton} onClick={handleGoBack}>
+            Go back
+          </button>
+          {movieDetails.poster_path && (
+            <img
+              className={MovieDetailsCss.img}
+              src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
+              alt={movieDetails.title}
+            />
+          )}
+        </div>
+        <div className={MovieDetailsCss.details}>
+          <h2>{movieDetails.title}</h2>
+          <p>User Score: {movieDetails.vote_average}</p>
+          <h3>Overview</h3>
+          <p>{movieDetails.overview}</p>
+          <h3>Genres</h3>
+          <p>{genres}</p>
+        </div>
       </div>
-      <div className={MovieDetailsCss.details}>
-        <h2>{movieDetails.title}</h2>
-        <p>User Score: {movieDetails.vote_average}</p>
-        <h3>Overview</h3>
-        <p>{movieDetails.overview}</p>
-        <h3>Genres</h3>
-        <p>{genres}</p>
-        </div>
-        </div>
       <div className={MovieDetailsCss.additionalInf}>
         <h3 className={MovieDetailsCss.additionalTitle}>Additional Information</h3>
         <ul>
@@ -68,10 +92,8 @@ const MovieDetails = () => {
           </li>
         </ul>
       </div>
-      </div>
-
+    </div>
   );
 };
 
 export default MovieDetails;
-
